4c6aee6a6a44685cc6e1cf219946d1ae430602a3
Dana Elrom <dana.elrom@post.runi.ac.il>
2023-12-27 21:15:15 +0200

    homework 2 - fix DamkaBoard extra space

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,12 +4,13 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
-		for(int i = 0; i < n; i++){
-			if(i % 2 != 0){
-			 	System.out.print(" ");
-			}
+		for(int i = 0; i < n; i++){			
 			for(int j = 0; j < n; j++){
-			  	System.out.print("* ");
+				if(i % 2 != 0){
+				  System.out.print(" *");
+				} else {
+				  System.out.print("* ");
+				}			  	
 			}
 
             System.out.println("");
